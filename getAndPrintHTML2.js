a8d31d94da806ee5cdf4bf8e3aab848ab04f3380
Alexandra Sia <alexandrasia@gmail.com>
2017-05-08 19:17:03 +0000

    Changed to print with parameters

---
+++ getAndPrintHTML2.js
@@ -0,0 +1,26 @@
+var https = require('https');
+
+function getAndPrintHTML(options) {
+
+  var allData = '';
+
+  https.get(options, function (response) {
+
+    response.setEncoding('utf8');
+
+    response.on('data', function (data) {
+
+      allData += data;
+      console.log(allData + '\n');
+    });
+
+    response.on('end', function () {
+      console.log('Response stream complete.');
+    });
+  });
+}
+
+getAndPrintHTML({
+  host: 'sytantris.github.io',
+  path: '/http-examples/step3.html'
+});
